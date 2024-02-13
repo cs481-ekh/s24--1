61b1944dd96cb7a750428ecfb5aac53ecc591c89
samuelflock <43181376+samuelflock@users.noreply.github.com>
2024-02-13 13:06:25 -0700

    Build Blank GUI Page Fixes #45

---
+++ window.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+
+# Starts Window
+window = tk.Tk()
+
+# Formats Window
+window.title('Descent')
+window.geometry('500x350')
+window.eval('tk::PlaceWindow . center')
+
+# Keeps Window Open
+window.mainloop()
