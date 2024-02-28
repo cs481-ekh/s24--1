49a7f348c535f8ccf85ff976cddda5eca939625f
zanemiller148 <97864776+zanemiller148@users.noreply.github.com>
2024-02-27 22:39:41 -0700

    #64 (#65)
    Add basic command line argument reading.

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
 # Main Python File
 # (Should Start from CLI and run window.py if GUI is chosen)
+
+import sys
+
+# Parameter flags
+gui_enable = False
+
+# Read command line arguments
+argc = len(sys.argv)
+print(f"{argc} arguments read. Arguments:", file=sys.stderr)
+
+for v in sys.argv:
+    print(v, file=sys.stderr, end="")
+print("", file=sys.stderr)
+
+if gui_enable:
+    print(f"GUI flag enabled, starting GUI...", file=sys.stderr)
