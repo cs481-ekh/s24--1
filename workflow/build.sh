a4a456ebb1e4029115fc453f10d2680a55e2f4a4
zanemiller148 <zanemiller148@u.boisestate.edu>
2024-03-06 22:49:08 -0700

    Move libgtk-3-dev to be a Ubuntu install.

--- workflow/build.sh
+++ workflow/build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-# Install dependencies
+# Install Ubuntu dependencies
+sudo apt-get install libgtk-3-dev
+
+# Install python dependencies
 #pip install -r ./workflow/requirements.txt
 
 # Run main.py
