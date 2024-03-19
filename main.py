1aac5ce3bf3f6f7818bdcd73fb3ae40fc83a9fb3
zanemiller148 <97864776+zanemiller148@users.noreply.github.com>
2024-03-19 16:43:32 -0600

    [#80] GitHub Actions Resolution (#86)
    * Only test print in main to explore segfault.
    
    * No functions calls in main.
    
    * Removed Descent.py import.
    
    * Added pytest to requirements.txt
    
    * Add test.bat
    
    * Added debug statements

--- main.py
+++ main.py
@@ -2,59 +2,63 @@
 # (Should Start from CLI and run window.py if GUI is chosen)
 
 import sys
-from Descent import *
+#from Descent import *
 import os
 
 def print_usage():
-    print(f"Descent is a genealogical analysis program that takes in files",file=sys.stdout)
-    print(f"in the format of .csv and presents genealogical output.", file=sys.stdout)
+    return "Descent is a genealogical analysis program that takes in files \nin the format of .csv and presents genealogical output."
 
 def print_details():
-    print_usage()
-    print(f"TODO: The list of options for each flag would be listed here.", file=sys.stdout)
+    return print_usage() + "\nTODO: The list of options for each flag would be listed here."
 
 def select_calc_option(str):
     print(f"TODO: Calculation option {str} would be selected here.", file=sys.stdout)
 
 def select_out_option(str):
     print(f"TODO: Output option {str} would be selected here.", file=sys.stdout)
-    
+
 # Parameter flags and variables
 gui_enable = False
 
-# Read command line arguments
-argc = len(sys.argv)
-print(f"{argc} arguments read.", file=sys.stderr)
+def cli_init(str_list):
+    # Read command line arguments
+    retVal = ""
+    argc = len(str_list)
+    print(f"{argc} arguments read.", file=sys.stderr)
 
-# Parse command line arguments
-for v in range(len(sys.argv)):
-    if "--g" in sys.argv[v]:
-        gui_enable = True
-    elif "--i" in sys.argv[v]:
-        if len(sys.argv) >= v:
-            print("--i flag requires a valid filename.", file=sys.stderr)
-        else:
-            input_filename = sys.argv[v + 1]
-            if os.path.isfile(input_filename):
-                input_file = open(input_filename)
+    # Parse command line arguments
+    for v in range(len(str_list)):
+        if "--h" in str_list[v]:
+            retVal = print_usage()
+            break
+        elif "--hf" in str_list[v]:
+            retVal = print_details()
+            break
+        elif "--i" in str_list[v]:
+            if v >= (len(str_list) + 1):
+                retVal += "--i flag requires a valid filename." + "\n"
             else:
-                print(f"{input_filename} could not be found and was not opened.", file=sys.stderr)
+                input_filename = str_list[v + 1]
+                if os.path.isfile(input_filename):
+                    input_file = open(input_filename)
+                else:
+                    retVal += f"{input_filename} could not be found and was not opened.\n"
 
-    elif "--hf" in sys.argv[v]:
-        print_details()
-    elif "--h" in sys.argv[v]:
-        print_usage()
-    elif "--c" in sys.argv[v]:
-        if len(sys.argv) >= v:
-            print("--c flag requires a valid calculation option.", file=sys.stderr)
-        else:
-            select_calc_option(sys.argv[v + 1])
-    elif "--o" in sys.argv[v]:
-        if len(sys.argv) >= v:
-            print("--o flag requires a valid output option.", file=sys.stderr)
-        else:
-            select_out_option(sys.argv[v + 1])
+        elif "--g" in str_list[v]:
+            gui_enable = True
+        elif "--c" in str_list[v]:
+            if len(str_list) >= v:
+                retVal += "--c flag requires a valid calculation option."
+            else:
+                select_calc_option(str_list[v + 1])
+        elif "--o" in str_list[v]:
+            if len(str_list) >= v:
+                retVal += "--o flag requires a valid output option."
+            else:
+                select_out_option(str_list[v + 1])
+    return retVal
 
+print(cli_init(sys.argv))
 if gui_enable:
     print(f"GUI flag enabled, starting GUI...", file=sys.stderr)
-    BuildApp()
+    #BuildApp()
