dfb2d68531d1d767f5ee9293743534221b89ec32
samuelflock <43181376+samuelflock@users.noreply.github.com>
2024-03-19 18:41:18 -0600

    Simple Validation and Exception Handling

--- DataManager.py
+++ DataManager.py
@@ -1,15 +1,25 @@
+
+
 class DataManager:
     # Initializes DataManager using Input File
     def __init__(self, file):
-        self.data = []
-        with open(file, 'r') as f:
-            temp = f.readlines()
-        #sepchar = self.determineSepChar(temp)
-        for line in temp:
-            #line = line.strip().split(sepchar)
-            line = line.strip().split(',') # CSV Specific
-            self.data.append([item.strip() for item in line])
-        print(self.data)
+        try:
+            self.data = []
+            if file.endswith('.csv'): # CSV Only
+                with open(file, 'r') as f:
+                    temp = f.readlines()
+                #sepchar = self.determineSepChar(temp)
+                for line in temp:
+                    #line = line.strip().split(sepchar)
+                    line = line.strip().split(',') # CSV Specific
+                    self.data.append([item.strip() for item in line])
+                print(self.data)
+            else:
+                print("The DataManager only accepts CSV as a Valid File Format")
+            pass
+        except:
+            print("There was a problem Initializing the DataManager")
+            return
 
 # Reimplement if not Ziker ever uses Non-CSV Files
     # def determineSepChar(self, d):
