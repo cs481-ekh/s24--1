6eb67c5f61c2705fa86ae0ff777fdef010bd1ce8
samuelflock <43181376+samuelflock@users.noreply.github.com>
2024-03-19 18:33:18 -0600

    Parser works for CSV Only

--- DataManager.py
+++ DataManager.py
@@ -1,21 +1,24 @@
 class DataManager:
+    # Initializes DataManager using Input File
     def __init__(self, file):
         self.data = []
         with open(file, 'r') as f:
             temp = f.readlines()
-        sepchar = self.determineSepChar(temp)
+        #sepchar = self.determineSepChar(temp)
         for line in temp:
-            line = line.strip().split(sepchar)
+            #line = line.strip().split(sepchar)
+            line = line.strip().split(',') # CSV Specific
             self.data.append([item.strip() for item in line])
         print(self.data)
 
-    def determineSepChar(self, d):
-        sepchars = ['\t', ',', ' ']
-        counts = {s: [] for s in sepchars}
-        for line in d[:10]:
-            for sep in sepchars:
-                counts[sep].append(line.count(sep))
-        for sep, count_list in counts.items():
-            if count_list[0] != 0 and count_list.count(count_list[0]) == len(count_list):
-                return sep
-        return None
+# Reimplement if not Ziker ever uses Non-CSV Files
+    # def determineSepChar(self, d):
+    #     sepchars = ['\t', ',', ' ']
+    #     counts = {s: [] for s in sepchars}
+    #     for line in d[:100]:
+    #         for sep in sepchars:
+    #             counts[sep].append(line.count(sep))
+    #     for sep, count_list in counts.items():
+    #         if count_list[0] != 0 and count_list.count(count_list[0]) == len(count_list):
+    #             return sep
+    #     return None
