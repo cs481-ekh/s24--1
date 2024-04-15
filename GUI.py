ea52036ab42ddcc75ba41f06648c03de18b290dc
samuelflock <43181376+samuelflock@users.noreply.github.com>
2024-04-15 14:25:30 -0600

    Revert "Toggleable Button Value"
    This reverts commit bc2a4deb3a03a365a3307c717151885bf64dc927.

--- GUI.py
+++ GUI.py
@@ -144,7 +144,6 @@ class EditorPanel(tk.Frame):
     def __init__(self, parent):
         super().__init__(parent)
         self.parent = parent
-        self.boolFirstRow = BooleanVar()
 
         # Create widgets
         self.create_panel_layout()
@@ -175,10 +174,9 @@ class EditorPanel(tk.Frame):
                                 showtoolbar=False, showstatusbar=True)
         pt.show()
 
-    # Displays Dropdown Menus, Check Boxes, Text Entries, and Check Errors Button
     def load_selection_menu(self, df):
         # Contains Header Checkbox
-        containsHeading = Checkbutton(self.selection_pane, text="Row one contains column headings", variable=self.boolFirstRow, command=self.toggle_first_row_header)
+        containsHeading = Checkbutton(self.selection_pane, text="Row one contains column headings")
         containsHeading.grid(row=0, column=0, columnspan=4)
 
         # Check for Incest Checkbox
@@ -287,21 +285,10 @@ class EditorPanel(tk.Frame):
 
         text = Text(self.error_pane, yscrollcommand=v.set)
 
-        global data_manager
-        errors = data_manager.checkForErrors()
-
         for e in errors:
             text.insert(END, e + "\n\n")
 
         text.pack()
-        pass
-
-    def toggle_first_row_header(self):
-        
-        if self.boolFirstRow.get(): # Is Checked
-            pass
-        else: # Not Checked
-            pass
         pass
 
 class RelatednessPanel(tk.Frame):
